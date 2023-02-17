10c46190bf81d47827723ac80d5bbc437c21c1a5
collinskrotich <kolinsr@gmail.com>
2023-02-17 09:41:16 -0800

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints if number is positive, negative or 0
+ * Return: Always 0
+ */
+int main(void) /* main function */
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
