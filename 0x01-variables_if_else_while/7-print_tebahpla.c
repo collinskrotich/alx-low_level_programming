8e6aaa3bb5b25cdc264c8bf1d0e0334f8ca29627
collinskrotich <kolinsr@gmail.com>
2023-02-17 10:31:31 -0800

    a program that prints the lowercase alphabet in reverse, followed by a new line.

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void) /* main function */
+{
+	char lc;
+
+	for (lc = 'z'; lc <= 'a'; lc--)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
+~
+
