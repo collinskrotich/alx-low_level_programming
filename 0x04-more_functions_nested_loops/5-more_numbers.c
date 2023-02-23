c2fec4842f4ea5efa5f4e87249ac854edf6bd45f
Collins Rotich <kolinsr@gmail.com>
2023-02-23 14:14:39 -0800

    Testing no.5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,18 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 10; i++)
+	for (j = 0; j <= 10; j++)
 	{
-		for (j = 0; j <= 14; j++)
+		i = 0;
+		while (i <= 14)
 		{
-			_putchar(i + '0');
+			if (i >= 10)
+			{
+				_putchar(i / 10 = '0');
+			}
+			_putchar(i % 10 + '0');
+			i++;
 		}
-	}
-	_putchar('\n');
+		_putchar('\n');
+}
 }
